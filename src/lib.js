b4bcd470919688046a2cf37a1d5269101d64dfa9
Tigre-sys968 <oros796@gmail.com>
2026-08-30 22:12:21 -0400

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnon = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;;
 export const apiUrl = import.meta.env.VITE_API_URL;
 
 export const supabase = createClient(supabaseUrl, supabaseAnon);
